3afde046d2e8d5601299350e3d3ce6f34ea3c042
Zaineb Qadiri <zqadiri@e1r2p2.1337.ma>
2022-04-01 11:23:53 +0000

    clean branch

--- exam_rank_04/microshell/shell.c
+++ exam_rank_04/microshell/shell.c
@@ -31,7 +31,7 @@ int	main(int argc, char *argv[], char **envp)
 			//!----- Parse cmd to the "|" or semi ";" -----!//
 			char 	*cmds[1000];
 			int		arg_nb = 0;
-			while(i < argc && strcmp(argv[i], "|") && strcmp(argv[i], ";"))
+			while(i < argc && strncmp(argv[i], "|", 2) && strncmp(argv[i], ";", 2))
 			{
 				cmds[arg_nb] = argv[i];
 				i++;
@@ -39,21 +39,18 @@ int	main(int argc, char *argv[], char **envp)
 			}
 			cmds[arg_nb] = 0;
 			//!---- Check for empty cmd ";" ";"	----!//
-			if (arg_nb == 0)
-			{
+			if (arg_nb == 0){
 				i++;
 				continue;
 			}
 			//!---- if the cmd is a pipeline -----!//
-			if (i < argc && !strcmp(argv[i], "|"))
+			if (i < argc && !strncmp(argv[i], "|", 2))
 			{
 				pipe(pipe_fd);
 				pipe_in = pipe_fd[0];
 				out = pipe_fd[1];
 				i++;
 			}
-			if (!strcmp(cmds[0], "cd"))
-				puts("cd");
 			else
 			{
 				// printf ("in : %d pipe_in : %d out : %d cmd : %s\n", in, pipe_in,out, cmds[0]);
@@ -68,7 +65,18 @@ int	main(int argc, char *argv[], char **envp)
 						close(in);
 					if (out != 1)
 						close(out);
-					if (execve(cmds[0], cmds, envp))
+					if (!strncmp(cmds[0], "cd", 3))
+					{
+						if (arg_nb != 2){
+							write (2, "error: cd: bad arguments\n", 25);
+							// return 1;
+						}
+						if (chdir(cmds[1])){
+							write (2, "error: cd: cannot change directory to\n", 38);
+							// return 1;
+						}
+					}
+					else if (execve(cmds[0], cmds, envp))
 						exit(126);
 				}
 				else
@@ -83,11 +91,9 @@ int	main(int argc, char *argv[], char **envp)
 				int status;
 				if (i >= argc || (i < argc && (!strcmp(argv[i], ";"))))
 				{
-					while(waitpid(-1, &status, 0) > 0)
-					{
-						// waitpid(pids[pid_index], 0, 0);
-						// pid_index--;
-					}
+					// while(waitpid(-1, &status, 0) > 0)
+					// 	;
+					waitpid(pids[pid_index], NULL, 0);
 					in = 0;
 					pipe_in = 0;
 					out = 1;
@@ -96,4 +102,61 @@ int	main(int argc, char *argv[], char **envp)
 			}
 		}
 	}
+}
+
+int main (int ac, char **av, char **env)
+{
+	int i = 1, j,k,l;
+	int p[2];
+	int fd_in;
+	pid_t pid;
+	while (i < ac)
+	{
+		j = i, k = i, l = i;
+		fd_in = 0;
+		while (j < ac && strncmp(av[j], ";", 2))
+			j++;
+		while (k < j)
+		{
+			l = k;
+			while (l < j && strncmp(av[l], "|", 2))
+				l++;
+			char *argv[l - k + 1];
+			sub();
+			pipe(p);
+			if ((pid = fork()) == -1)
+				return -1;
+			else if (pid == 0)
+			{
+				dup2(fd_in, 0);
+				if (l < j)
+					dup2(p[1], 1);
+				close (p[0]);
+				if (!strncmp(argv[0], "cd", 3))
+					cd(argv, l - k);
+				else if (execve(argv[0], argv, env))
+				{
+					close (p[1]);
+					close (fd_in);
+					return -1;
+				}
+				close (p[1]);
+				close (fd_in);
+				return 0;
+			}
+			else
+			{
+
+				waitpid(pid, NULL, 0);
+				close (p[1]);
+				if (fd_in)
+					close(fd_in);
+				fd_in = p[0];
+			}
+			k = l + 1;
+		}
+		i = j +1;
+		close (fd_in);
+	}
+	return 0;
 }
